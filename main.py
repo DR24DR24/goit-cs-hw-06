8ae89b662942b1843408a7adb573be9f28fc9a06
rd <snob24zp@proton.me>
2025-03-24 00:18:43 +0200

    work

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import socketserver
 from websockets import WebSocketServerProtocol
 from websockets.exceptions import ConnectionClosedOK
 import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
 
 # Конфігурація
 HTTP_PORT = 3000
@@ -18,29 +20,64 @@ MONGO_URI = "mongodb://mongo:27017/chatdb"
 DB_NAME = "chatdb"
 COLLECTION_NAME = "messages"
 
-
-
 logging.basicConfig(level=logging.INFO)
 
 # HTTP-сервер
-class HTTPServerHandler(http.server.SimpleHTTPRequestHandler):
+class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/":
-            self.path = "/index.html"
-        elif self.path == "/message":
-            self.path = "/message.html"
-        elif self.path == "/style.css":
-            self.path = "/style.css"
-        elif self.path == "/logo.png":
-            self.path = "/logo.png"
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == "/":
+            self.send_html_file("index.html")
+        elif pr_url.path == "/message.html":
+            self.send_html_file("message.html")
+        elif pr_url.path == "/style.css":
+            self.send_html_file("style.css")
+        elif pr_url.path == "/logo.png":
+            self.send_html_file("logo.png")
         else:
-            self.path = "/error.html"
-        return super().do_GET()
+            self.send_html_file("error.html", 404)
+            
+    def do_POST(self):
+        if self.path == "/message":
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode("utf-8")
+            post_params = urllib.parse.parse_qs(post_data)
+            
+            username = post_params.get("username", [""])[0]
+            message = post_params.get("message", [""])[0]
+            data = {"date": datetime.now().isoformat(), "username": username, "message": message}
+            
+            try:
+                client = MongoClient(MONGO_URI)
+                db = client[DB_NAME]
+                collection = db[COLLECTION_NAME]
+                collection.insert_one(data)
+                self.send_response(200)
+            except errors.PyMongoError as e:
+                logging.error(f"Error inserting data into MongoDB: {e}")
+                self.send_response(500)
+            
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Message received and stored.")        
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        try:
+            with open(filename, "rb") as fd:
+                self.wfile.write(fd.read())
+        except FileNotFoundError:
+            self.send_response(404)
+            self.end_headers()
 
 def start_http_server():
-    with socketserver.TCPServer(("", HTTP_PORT), HTTPServerHandler) as httpd:
-        logging.info(f"HTTP Server running on port {HTTP_PORT}")
-        httpd.serve_forever()
+    server_address = ("", HTTP_PORT)
+    httpd = HTTPServer(server_address, HttpHandler)
+    logging.info(f"HTTP Server running on port {HTTP_PORT}")
+    httpd.serve_forever()
+
 
 class Server:
     clients = set()
@@ -90,4 +127,5 @@ async def main():
 
 if __name__ == '__main__':
     threading.Thread(target=start_http_server, daemon=True).start()
+    #threading.Thread(target=start_http_server, daemon=True).start()
     asyncio.run(main())
